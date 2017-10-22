152ff9ce013cefe3f3486e88b4987d450500dffc
Jacob Hagstedt <jacob@textual.se>
2017-10-22 10:18:13 +0200

    Add edit of result and minor postprocessing

--- flask/app.py
+++ flask/app.py
@@ -25,9 +25,12 @@ class QA(db.Model):
     answer = db.Column(db.String(50), nullable=False)
     vote = db.Column(db.Integer, nullable=False)
     ip = db.Column(db.String(50), nullable=False)
+    changed = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<IP %r>' % self.ip
+        return "id={}, q={}, a={}, ip={}".format(self.id, self.question, self.answer, self.ip)
+
+    __str__ = __repr__
 
 # Make sure the db is created
 db.create_all()
@@ -37,17 +40,34 @@ def vote():
     try:
         is_up = 'up' in request.form
         qa_id = request.form.get('qa_id', None)
+        question = request.form.get('question').strip().lower()
+        answer = request.form.get('answer').strip().lower()
 
         if qa_id is None:
             return redirect("/", code=302)
 
         # Load object
         qa = QA.query.get(int(qa_id))
-        qa.vote = 1 if is_up else -1
+        # Check if the question or answer has changed, if so,
+        # its an upvote directly
+        q_diff = qa.question != question
+        a_diff = qa.answer != answer
+
+        qa.question = question
+        qa.answer = answer
+        
+        if q_diff or a_diff:
+            qa.vote = 1
+            qa.changed = 1
+        else:
+            qa.vote = 1 if is_up else -1
+            qa.changed = 0
+
+        print("Saving", qa)
         db.session.commit()
-    except Exception:
+    except Exception as e:
+        print("Error saving:", e)
         # TODO: Log to db
-        pass
 
     return redirect("/", code=302)
 
@@ -67,7 +87,7 @@ def main():
         ans = generate_answer(question.lower())
 
         # Create a QA object that can be saved
-        qa = QA(question=question, answer=ans, vote=0, ip=request.remote_addr)
+        qa = QA(question=question.strip().lower(), answer=ans.strip().lower(), vote=0, ip=request.remote_addr, changed=0)
         db.session.add(qa)
         db.session.commit()
         qa_id = qa.id
