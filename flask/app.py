4dd7c45fad3821bc09c1001c62e6f49ca99d23f3
Jacob Hagstedt <jacob@textual.se>
2017-10-22 11:25:59 +0200

    Add redirect route so results can be shared, fixed but where vote would overwrite instead of update

--- flask/app.py
+++ flask/app.py
@@ -38,38 +38,59 @@ db.create_all()
 @app.route('/vote', methods=['POST'])
 def vote():
     try:
+        # Get the data
+        # How did the user vote
         is_up = 'up' in request.form
+        # Id of the result
         qa_id = request.form.get('qa_id', None)
+        # The maybe updated question
         question = request.form.get('question').strip().lower()
+        # The maybe updated answer
         answer = request.form.get('answer').strip().lower()
 
+        # sanity if we have an id
         if qa_id is None:
             return redirect("/", code=302)
 
-        # Load object
+        # Load object using the id
         qa = QA.query.get(int(qa_id))
-        # Check if the question or answer has changed, if so,
-        # its an upvote directly
+        # Check if the question or answer has changed,
+        # if so, it's directly an upvote
         q_diff = qa.question != question
         a_diff = qa.answer != answer
 
+        # Set the question and answer
         qa.question = question
         qa.answer = answer
         
+        # Set the vote and change flag
         if q_diff or a_diff:
-            qa.vote = 1
+            qa.vote += 1
             qa.changed = 1
         else:
-            qa.vote = 1 if is_up else -1
+            qa.vote += 1 if is_up else -1
             qa.changed = 0
 
-        print("Saving", qa)
+        # Commit changes to db
         db.session.commit()
     except Exception as e:
         print("Error saving:", e)
         # TODO: Log to db
 
     return redirect("/", code=302)
+
+
+@app.route('/<int:qa_id>', methods=['GET'])
+def show_result(qa_id):
+    # Load from the db
+    qa = QA.query.get(int(qa_id))
+
+    if qa is None:
+        return redirect('/', code=302)
+
+    # Serve the result
+    return render_template('index.html', answer=qa.answer, question=qa.question, qa_id=qa_id)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -82,6 +103,7 @@ def main():
         question = request.args.to_dict().get('question', None)
 
     if question is None:
+        # We have no question, so just serve the index site
         return render_template('index.html')
     else:
         ans = generate_answer(question.lower())
@@ -92,7 +114,8 @@ def main():
         db.session.commit()
         qa_id = qa.id
 
-        return render_template('index.html', answer=ans, question=question, qa_id=qa_id)
+        # Redirect to the specific URL so that the user can share the result bcs fun!
+        return redirect('/{}'.format(qa_id), code=302)
 
 
 if __name__ == '__main__':
